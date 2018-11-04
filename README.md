a28c8cbf604779e39e496d308ca354cf5179f974
webdev-gmg <41268601+webdev-gmg@users.noreply.github.com>
2018-11-03 23:46:49 -0400

    Commit Changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Train-Schedule
+
+This application takes in the First Run time of the train and calculates 
+    #The train arrival time
+    #Minutes to Arrival
+
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAuF5axKytWph_D4aAfb0QTnDTyeFDzRus",
+    authDomain: "trainproject-b87eb.firebaseapp.com",
+    databaseURL: "https://trainproject-b87eb.firebaseio.com",
+    projectId: "trainproject-b87eb",
+    storageBucket: "trainproject-b87eb.appspot.com",
+    messagingSenderId: "800988815225"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#newTrain").val().trim();
+    var destination = $("#destination").val().trim();
+    var frequency = $("#frequency").val().trim();
+    var arrival = $("#arrival").val().trim();
+    var firstRun = $("#firstRun").val();
+    database.ref().push({
+        firstRun:firstRun,
+        name: name,
+        destination: destination,
+        frequency: frequency,
+        arrival: arrival
+    })
+})
+
+
+
+
+database.ref().on("child_added", function(snapshot) {
+    var firstTime = snapshot.val().firstRun;
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+    
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+    
+        // Time apart (remainder)
+        var tRemainder = diffTime % snapshot.val().frequency;
+        console.log(tRemainder);
+    
+        // Minute Until Train
+        var tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        nxtTrain = moment(nextTrain).format("hh:mm");
+
+    $("tbody").append("<tr><th>" + snapshot.val().name + "</th><th>" + snapshot.val().destination + "</th><th>" + snapshot.val().frequency + "</th><th>" + nxtTrain + "</th><th>" + tMinutesTillTrain + "</th><th>" + '-' + "</th></tr>")
+})
